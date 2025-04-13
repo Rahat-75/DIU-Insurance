56bf715daf5519845a2754d093a7c1c40a347869
Rahat-75 <lrabbi15-4583@diu.edu.bd>
2025-04-14 01:29:35 +0600

    final commit

---
+++ Script/db/config.php
@@ -0,0 +1,9 @@
+<?php
+DEFINE('DBHost','localhost');
+DEFINE('DBUser', 'root');
+DEFINE('DBPass','');
+DEFINE('DBName','E-Insurance');
+DEFINE('DBCharset','utf8mb4');
+DEFINE('DBCollation', 'utf8_general_ci');
+DEFINE('DBPrefix', '');
+?>
